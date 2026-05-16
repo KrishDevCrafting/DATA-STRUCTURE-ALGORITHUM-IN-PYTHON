a19b88cb40a5e75ea66c85ce305ade37d94f1993
KrishDevCrafting <krishkumer150@gmail.com>
2026-05-16 21:47:59 +0530

    updated

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-import pyjokes
+# import pyjokes
 
-variable = pyjokes.get_joke()
+# variable = pyjokes.get_joke()
 
-print(variable)
+# print(variable)
 
 
 a = 5
@@ -17,4 +17,80 @@ b2 = 80
 
 print(a1>b2)
 
+Name = "KRISH"
 
+shortName = Name[0:3]
+
+print(shortName)
+
+
+value = "Hey \" Krish\" what's going \n on did you finish your homework or not?"
+
+print(value)
+
+# name = input("Enter your Name plz..")
+
+# print(f"Hii My Handsome {name}")
+
+word = "Krish"
+
+print(word[1:6:2])
+
+
+arry = ["krish",1,0.25,True,'Apple'];
+
+arry.insert(3, "Napolean-Hill")
+print(arry)
+
+
+ab = (1,)
+
+print(type(ab))
+
+
+
+fruits = []
+
+
+# f1 = input("Enter the fruits..")
+# fruits.append(f1)
+
+
+
+# for i in range(5):
+#     print(i)
+
+
+# for y in range(1,11):
+#     if y % 2 == 1:
+#         print(y)
+# for x in range(0,7):
+#     f1 = input("Enter the Fruits..")
+#     fruits.append(f1)
+#     print(fruits)
+
+
+
+# numberOfstudents = []
+
+# for marks in range(0,6):
+#  ab1 = input("Enter the Marks of the Studens!")
+#  numberOfstudents.append(ab1)
+
+#  numberOfstudents.sort()
+
+#  print(numberOfstudents)
+
+marks = {
+    "KRISH" : 99,
+    "Gender" : "Male",
+    "Expertise" : "Racing Racing aur Cars++"
+
+}
+
+
+print(marks.items())
+print(marks.values())
+
+s = set()
+print(s)
