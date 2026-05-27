e5c2866100c16f8b0cc5df4c2b068ba5790aa3fc
Krish <krishkumer150@gmail.com>
2026-05-28 00:35:50 +0530

    Factorial

--- index.py
+++ index.py
@@ -334,6 +334,14 @@ for i in range(1,n + 1):
 
 
 
+# Factorial using Recursion
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+print(f"The factorial of {n} is {factorial(5)}")
 
 
 
