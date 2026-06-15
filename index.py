a266a4ce52b6ecbd043cf6f416f7caf5352d5bc8
KrishDevCrafting <krishkumer150@gmail.com>
2026-06-15 22:04:54 +0530

    updated

--- index.py
+++ index.py
@@ -326,15 +326,14 @@ else:
 #  ***
 # *****
 # n = int(input("Enter the star number!"))
-<<<<<<< HEAD
-=======
+
 
 # for i in range(1,n + 1):
 #     print(" " * (n - i), end="")
 #     print("*" * (2 * i- 1), end="" )
 #     print("")
 
->>>>>>> master
+
 
 # for i in range(1,n + 1):
 #     print(" " * (n - i), end="")
