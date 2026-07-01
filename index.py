aadd4150b233c6b5e34f87477461d2a14cb63434
Krish <krishkumer150@gmail.com>
2026-07-01 22:35:20 +0530

    updated

--- index.py
+++ index.py
@@ -333,10 +333,7 @@ else:
 #     print("*" * (2 * i- 1), end="" )
 #     print("")
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 
 # for i in range(1,n + 1):
 #     print(" " * (n - i), end="")
