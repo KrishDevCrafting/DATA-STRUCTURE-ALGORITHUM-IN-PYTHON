cf3ff9256fb026b558f094c6513939e5106d2fe2
KrishDevCrafting <krishkumer150@gmail.com>
2026-07-13 13:28:03 +0530

    updated

--- index.py
+++ index.py
@@ -453,3 +453,18 @@ def game():
     return score
 
 game()
+ 
+
+#  Write a program to generate multiplication tables from 2 to 20 and write it to the different files. place these files in a folder for a 13 years_old_child..
+
+def generateTable(n):
+    table = ""
+    for i in range(1, 11):
+        table += f"{n}X{i}={n*i}\n"
+
+    with open(f"tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+
+for n in range(2, 21):
+    generateTable(n)
