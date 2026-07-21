887b2f2a0dd1c8eb3d7181f7b6e8b3a4bc716dc2
Krish <krishkumer150@gmail.com>
2026-07-21 23:05:41 +0530

    updated

--- index.py
+++ index.py
@@ -732,17 +732,24 @@ print(e.increment)
 
 # Wrtie a class 'complex' to represent complex numbers, along with overloaded operations "+" and "*" operator which calculates the sum nd the dot(.) product of them....
 
-class comlex:
-    def __init__(self,r,i):
+class Complex:
+    def __init__(self, r, i):
         self.r = r
         self.i = i
 
-    def __add__(self,c2):
-        return complex(self.r + c2.r,self.i+ c2.i)
+    def __add__(self, c2):
+        # Return a new instance of Complex, not Python's built-in complex
+        return Complex(self.r + c2.r, self.i + c2.i)
+
+    def __mul__(self, c2):
+        real_part = self.r * c2.r - self.i * c2.i
+        imag_part = self.r * c2.i + self.i * c2.r
+        return Complex(real_part, imag_part)
 
     def __str__(self):
         return f"{self.r} + {self.i}i"
 
-c1 = comlex(1,2)
-c2 = comlex(3,4)
-print(c1 + c2 )       
+c1 = Complex(1, 2)
+c2 = Complex(3, 4)
+print(c1 + c2)  # Output: 4 + 6i
+print(c1 * c2)  # Output: -5 + 10i
