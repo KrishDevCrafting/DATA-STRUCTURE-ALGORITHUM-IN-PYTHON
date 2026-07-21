93cfc9139583f76488d4e99f834daa7cf11af315
KrishDevCrafting <krishkumer150@gmail.com>
2026-07-21 21:42:04 +0530

    updated

--- index.py
+++ index.py
@@ -712,5 +712,39 @@ d = Dog()
 d.bark()        
 
 # 3 create a class 'Employee' and add salary and increment properties to it..
+# Write a method "salaryAfterIncrement" method with a @property decorator with a setter which changes the value of increment based on the salary...
+class Employee:
+    salary = 234
+    increment = 20
+
+    @property
+    def salaryAfterIncrement(self):
+        return self.salary + self.salary * (self.increment / 100)
+
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, salary):
+        self.increment = ((salary / self.salary) - 1) * 100
 
 
+e = Employee()
+print(e.salaryAfterIncrement)
+
+e.salaryAfterIncrement = 280.8
+print(e.increment)
+
+# Wrtie a class 'complex' to represent complex numbers, along with overloaded operations "+" and "*" operator which calculates the sum nd the dot(.) product of them....
+
+class comlex:
+    def __init__(self,r,i):
+        self.r = r
+        self.i = i
+
+    def __add__(self,c2):
+        return complex(self.r + c2.r,self.i+ c2.i)
+
+    def __str__(self):
+        return f"{self.r} + {self.i}i"
+
+c1 = comlex(1,2)
+c2 = comlex(3,4)
+print(c1 + c2 )       
