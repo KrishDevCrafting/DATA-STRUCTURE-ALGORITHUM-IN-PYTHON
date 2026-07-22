66bcdbbd1c1ca1ddc278996065a16a52a7afc8dd
Krish <krishkumer150@gmail.com>
2026-07-22 14:54:14 +0530

    update

--- Project_2/main.py
+++ Project_2/main.py
@@ -44,3 +44,30 @@ while users := fetch_user_batch(page):
     print(f"Processing page {page}: {users}")
     page += 1
 
+class Person:
+    def __init__(self, initial_page: int =1):
+     
+     self.page = initial_page
+    
+     self.usersApi = {1:["Ishigami Senku" ,"Suika", "Gen Asagiri"], 2:["Tsukasa ShishiouZoro", "Hyoga"], 3:["Dr Xeno.."]}
+    
+    def fetch_user_batch(self) -> list[str]:
+       '''Fetches a batch of users from the simulated API.'''
+       batch = self.usersApi.get(self.page, [])
+       self.page += 1  # Move to the next page for the next call
+       return batch 
+    
+    # Initialize the Object.....
+fetcher = Person()
+
+    # Using the walrus operator to fetch and process users in a loop
+
+while users := fetcher.fetch_user_batch():
+        print(f"Processing page {fetcher.page - 1}: {users}") 
+
+# Types Definition in python:
+age: int = 25 
+
+def greeting(name: str) ->str:
+    return f"Hiiiiiiiiiiiiiiiiii,{name}"
+print(greeting("Radha...."))
