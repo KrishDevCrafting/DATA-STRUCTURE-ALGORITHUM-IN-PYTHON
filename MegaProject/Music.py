b24dbf36d8e2b7fa5bc83bb7592c961610459a4b
Krish <krishkumer150@gmail.com>
2026-07-31 14:50:42 +0530

    update

--- MegaProject/Music.py
+++ MegaProject/Music.py
@@ -1,5 +1,4 @@
 music = {
-    "Fall in love": "https://www.youtube.com/watch?v=TxvpctgU_s8&list=RDGMEMYH9CUrFO7CfLJpaD7UR85wVMTxvpctgU_s8&start_radio=1",
-    "Maan Meri Jaan": "https://www.youtube.com/watch?v=0wz8gk1x3sY&list=RDGMEMYH9CUrFO7CfLJpaD7UR85wVMTxvpctgU_s8&start_radio=1",
+    "fall in love": "https://www.youtube.com/watch?v=TxvpctgU_s8&list=RDGMEMYH9CUrFO7CfLJpaD7UR85wVMTxvpctgU_s8&start_radio=1",
     "Phonk": "https://www.youtube.com/watch?v=UhJiV8CQ-kA&list=RDUhJiV8CQ-kA&start_radio=1"
 }
